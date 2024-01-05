d77b29047ffddf9da8e7bd34dad01f78bb98e6e5
Matthew Port Louis <88897685+bilbaothanos6@users.noreply.github.com>
2024-01-05 16:48:39 -0600

    rudimentary login page works

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,32 +1,18 @@
 import React from 'react';
-import { auth, GoogleAuthProvider } from '../firebase/firebase'; // Adjust the path as needed
-import { signInWithPopup } from 'firebase/auth';
+import { useNavigate } from 'react-router-dom';
+import { signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
+import { auth } from '../firebase/firebase'; // Adjust the path as needed
 
 function Login() {
+  const navigate = useNavigate();
+
   const handleGoogleLogin = () => {
     const provider = new GoogleAuthProvider();
-
+    
     signInWithPopup(auth, provider)
       .then(result => {
-        // Get the user's ID token as it is needed to authenticate with the backend
-        result.user.getIdToken().then(idToken => {
-          // Send token to your backend via HTTPS
-          fetch('http://localhost:5000/secure-action', {
-            method: 'POST',
-            headers: {
-              'Authorization': idToken,
-              'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ /* data you want to send */ })
-          })
-          .then(response => response.json())
-          .then(data => {
-            console.log("Backend response:", data);
-          })
-          .catch(error => {
-            console.error("Error sending token to backend:", error);
-          });
-        });
+        // After successful login, navigate to the FitnessCalculator page
+        navigate('/fitness-calculator');
       })
       .catch(error => {
         console.error("Error signing in with Google:", error);
